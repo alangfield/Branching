981ff29019acb1e8e8ad95f1cdbf16aa026e0697
Alex <alangfield@mymail.mines.edu>
2020-02-28 20:20:45 -0700

    Adding cat ears to the file

--- DrawPlay/src/Cat.java
+++ DrawPlay/src/Cat.java
@@ -6,6 +6,8 @@ public class Cat {
 	// drawing constants are private - noone needs to know what we're doing
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 100;
+	//adding cat ears
+	private static final int EAR_SIZE = HEAD_DIMENSION/5;
 	// eyes will be about 1/4 from top of head and 1/4 from left
 	private static final int EYE_Y = HEAD_DIMENSION/4;
 	private static final int EYE_X = HEAD_DIMENSION/4;
@@ -77,5 +79,24 @@ public class Cat {
 					g2.drawLine(x, y, x2, y2);
 					y2 += WHISKER_SLANT; 			
 				}
+				// Add ears
+				g2.setColor(Color.LIGHT_GRAY);
+				// Draw left side of left ear
+				x = catX + HEAD_DIMENSION/5;
+				x2 = x + EAR_SIZE/2;
+				y = catY + HEAD_DIMENSION/5;
+				y2 = y - EAR_SIZE;
+				g2.drawLine(x, y, x2, y2);
+				// Draw right side of right ear
+				// Note that we reverse x/x2 and y/y2
+				x = x2 + EAR_SIZE/2;
+				g2.drawLine(x2, y2, x, y);
+				// Draw left side of right ear
+				x += HEAD_DIMENSION/5;
+				x2 = x + EAR_SIZE/2;
+				g2.drawLine(x2, y2, x, y);
+				// Draw right side of right ear
+				x = x2 + EAR_SIZE/2;
+				g2.drawLine(x2, y2, x, y);
 	}
 }
